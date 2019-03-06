cfffb4f6e80eb422e2e1e6133114c95024b137ed
longshenzhu <1223006618@qq.com>
2019-03-06 16:57:18 +0800

    add form authorize

--- webForVueApp/webForVueApp/Api/AcountController.cs
+++ webForVueApp/webForVueApp/Api/AcountController.cs
@@ -3,16 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
+using System.Web;
 using System.Web.Http;
+using System.Web.Http.Hosting;
+using System.Web.Security;
 using Common;
 
 namespace webForVueApp.Api
 {
     public class AccountController : ApiController
     {
-        public MessageResult LoginIn()
+        [HttpGet]
+        public MessageResult LoginIn(string loginName,string password)
         {
-            return MessageResult.CreateSuccessResult();
+            //string cookieValue = "";
+            //HttpCookie cookieGet = HttpContext.Current.Request.Cookies["userName"];
+            //HttpCookie cookieSet = new HttpCookie("username")
+            //{
+            //    Value = HttpUtility.UrlDecode(loginName, Encoding.GetEncoding("GB2312")),
+            //    Expires = DateTime.Now.AddMinutes(10)
+            //};
+            //HttpContext.Current.Response.Cookies.Add(cookieSet);
+            FormsAuthentication.SetAuthCookie(loginName,true);
+            return MessageResult.CreateSuccessResult("登录成功",new { loginName,password});
+        }
+
+        [HttpGet]
+        public MessageResult LoginOut()
+        {
+            FormsAuthentication.SignOut();
+            return MessageResult.CreateSuccessResult("注销成功");
+        }
+
+        [HttpGet]
+        public MessageResult GetAccountInfo()
+        {
+            var user = HttpContext.Current.User.Identity.Name;
+            //HttpCookie cookieGet = HttpContext.Current.Request.Cookies["userName"];
+            return MessageResult.CreateSuccessResult(data: user);
         }
 
     }
