a4f9b62fb1c395a7428083b8caf64ac81fb73ef5
longshenzhu <1223006618@qq.com>
2019-03-07 17:57:29 +0800

    add webapi config for cors

--- webForVueApp/webForVueApp/App_Start/WebApiConfig.cs
+++ webForVueApp/webForVueApp/App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Formatting;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace webForVueApp
 {
@@ -34,6 +35,12 @@ namespace webForVueApp
             //默认支持xml、json返回格式，去掉xml，在项目中可自定义输出格式
             GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
             //GlobalConfiguration.Configuration.Formatters.Add(new JsonMediaTypeFormatter());
+        }
+        public static void SetCors(HttpConfiguration config)
+        {
+            //https://msdn.microsoft.com/en-us/magazine/dn532203.aspx
+            //config.EnableCors();
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
         }
     }
 }
